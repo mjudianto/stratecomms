c88f1de3151e4f88a3b34eb86dc7e1728d583ce2
thiangeline19 <thiangeline19@gmail.com>
2022-10-03 13:39:36 +0700

    kerjaanthiaa nambah jadwal

--- src/components/static/js/index.js
+++ src/components/static/js/index.js
@@ -3,52 +3,48 @@ import styles from '../css/index.module.css'
 
 export function activeHome(){
   let home = <li className="nav-item">
-    <a className={`nav-link ${styles.aFontStyle}`} style={{textAlign: 'right', paddingRight: '12%'}} aria-current="page" href="/" >Home</a></li>;
+    <a className={`nav-link ${styles.aFontStyle}`} style={{textAlign: 'right'}} aria-current="page" href="/" >Home</a></li>;
 
-  if (window.location.href == 'http://localhost:3000/'){
+  if (window.location.href === 'http://localhost:3000/'){
     home = <li className="nav-item">
-    <a className={`nav-link ${styles.aFontStyle} ${styles.navbarActive}`} style={{textAlign: 'right', paddingRight: '12%'}} aria-current="page" href="/" >Home</a></li>
-    return home;
-  } else {
+    <a className={`nav-link ${styles.aFontStyle} ${styles.navbarActive}`} style={{textAlign: 'center'}} aria-current="page" href="/" >Home</a></li>
     return home;
   }
+    return home;
 }
 
 export function activeArtikel(){
   let artikel = <li className="nav-item">
     <a className={`nav-link ${styles.aFontStyle}`} href="/artikel">Artikel</a></li>;
 
-  if (window.location.href == 'http://localhost:3000/artikel'){
+  if (window.location.href === 'http://localhost:3000/artikel'){
     artikel = artikel = <li className="nav-item">
     <a className={`nav-link ${styles.aFontStyle} ${styles.navbarActive}`} href="/artikel">Artikel</a></li>;
     return artikel;
-  } else {
-    return artikel;
   }
+    return artikel;
 }
 
 export function activeJadwalSertifikasi (){
   let jadwalSertifikasi = <li className="nav-item">
-    <a className={`nav-link ${styles.aFontStyle}`} style={{width: '10vw'}} href="#">Jadwal Sertifikasi</a></li>;
+    <a className={`nav-link ${styles.aFontStyle}`} style={{width: '14vw'}} href="/jadwal">Jadwal Sertifikasi</a></li>;
 
-  if (window.location.href == 'http://localhost:3000/jadwalsertifikasi'){
+  if (window.location.href === 'http://localhost:3000/jadwalsertifikasi'){
     jadwalSertifikasi = <li className="nav-item">
     <a className={`nav-link ${styles.aFontStyle} ${styles.navbarActive}`} style={{width: '10vw'}} href="#">Jadwal Sertifikasi</a></li>;
     return jadwalSertifikasi;
-  } else {
-    return jadwalSertifikasi;
   }
+    return jadwalSertifikasi;
 }
 
 export function activePortofolio (){
   let portofolio = <li className="nav-item">
-    <a className={`nav-link ${styles.aFontStyle}`} style={{paddingLeft: '20%'}} href="#">Portofolio</a></li>;
+    <a className={`nav-link ${styles.aFontStyle}`} href="#">Portofolio</a></li>;
 
-  if (window.location.href == 'http://localhost:3000/portofolio'){
+  if (window.location.href === 'http://localhost:3000/portofolio'){
     portofolio = <li className="nav-item">
-    <a className={`nav-link ${styles.aFontStyle} ${styles.navbarActive}`} style={{paddingLeft: '20%'}} href="#">Portofolio</a></li>;
-    return portofolio;
-  } else {
+    <a className={`nav-link ${styles.aFontStyle} ${styles.navbarActive}`} href="#">Portofolio</a></li>;
     return portofolio;
   }
+    return portofolio;
 }
--- src/index.js
+++ src/index.js
@@ -12,6 +12,7 @@ import logo from './components/static/images/logo.png';
 import { BsInstagram, BsWhatsapp } from 'react-icons/bs';
 import { MdOutlineEmail } from 'react-icons/md';
 import * as jsFunction from './components/static/js/index.js';
+import Jadwal from './components/pages/Jadwal';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render( 
@@ -20,7 +21,7 @@ root.render(
       <div className="container-fluid"> 
         <div class="container-fluid">
           <a class="navbar-brand" href="/">
-            <img src={logo} alt="Logo" width="270px" height="35px"/>
+            <img src={logo} alt="Logo" width="150px" height="20px"/>
           </a>
         </div>
         <div className="collapse navbar-collapse" id="navbarNavDropdown">
@@ -30,7 +31,7 @@ root.render(
             {jsFunction.activeJadwalSertifikasi()}
             {jsFunction.  activePortofolio()}
             <button className={`btn ${styles.buttonPendaftaran}`}>
-              <a className={`nav-link ${styles.aFontStyle}`} href="/Pendaftaran">Pendaftaran</a>
+              <a className={`nav-link ${styles.aFontStyle}`} style={{fontSize: '10pt'}} href="/Pendaftaran"><b>PENDAFTARAN</b></a>
             </button>
           </ul>
         </div>
@@ -40,10 +41,11 @@ root.render(
       <Route exact path='/' element={<HomePage/>}/> 
       <Route path='/artikel' element={<Artikel/>}/> 
       <Route path='/pendaftaran' element={<Pendaftaran/>}/>
+      <Route path='/jadwal' element={<Jadwal />} />
     </Routes>
     <footer className={`${styles.footerBody}`}>
       <h1 className={`${styles.footerh1}`}>Kami Dekat Dengan Anda</h1>
-      <h4 className={`${styles.footerh4}`}><BsInstagram /> &nbsp; Instagram</h4>
+      <h4 className={`${styles.footerh4}`}><BsInstagram /> &nbsp; @stratecomms</h4>
       <h4 className={`${styles.footerh4}`}><BsWhatsapp /> &nbsp; 081390601398 (Adi)</h4>
       <h4 className={`${styles.footerh4}`}><MdOutlineEmail /> &nbsp; stratecomms@gmail.com</h4>
       <button className={`${styles.footerButton}`}>HUBUNGI KAMI</button>
