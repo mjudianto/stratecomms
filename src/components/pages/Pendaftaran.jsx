2f54daf6c147762ce7f4ab3c78080fdba8b77113
unknown <venny.535200006@stu.untar.ac.id>
2022-10-04 20:32:42 +0700

    pendaftaran

--- src/components/pages/Pendaftaran.jsx
+++ src/components/pages/Pendaftaran.jsx
@@ -6,48 +6,64 @@ import styles from '../static/css/pendaftaran.module.css'
 class Pendaftaran extends Component {
   state = {  } 
 
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    // console.log(user)
+
+    axios.post('../pendaftaran/post')
+      .then(() => console.log('post success')
+      , window.location.href = "/pendaftaranberhasil",
+      )
+      .catch(err => {
+        console.error(err);
+      });
+  };
+
   render() { 
     return (
       <React.Fragment>
+        <div className={styles.bghome}></div>
         <div className={`container-fluid ${styles.body}`}>
             <div class="mb-3 row text-center">
                 <h1>Pendaftaran</h1>
             </div>
-            <form class="form-horizontal">
+            <form class="form-horizontal" method='post' onSubmit={this.handleSubmit}>
                 <div class="row mb-3">
                     <label className={`control-label col-sm-2 ${styles.variabel}`}>Nama Lengkap</label>
                         <div className={`col-7 ${styles.column}`}>
-                            <input class="form-control" type="Nama Lengkap" />
+                            <input className={`form-control ${styles.size}`}  type="Nama Lengkap" />
                         </div>
                 </div>
                 <div class="row mb-3">
                     <label className={`control-label col-sm-2 ${styles.variabel}`}>Nomor Telepon (Whatsapp) </label>
                     <div className={`col-7 ${styles.column}`}>
-                            <input class="form-control" type="Nomor telepon" />
+                            <input className={`form-control ${styles.size}`} type="Nomor telepon" />
                         </div>
                 </div>
                 <div class="row mb-3">
                     <label className={`control-label col-sm-2 ${styles.variabel}`}>Scan Pas Foto (3x4) </label>
                     <div className={`col-7 ${styles.column}`}>
-                            <input class="form-control" type="file"/>
+                            <input className={`form-control ${styles.size}`}  type="file"/>
                         </div>
                 </div>
                 <div class="row mb-3">
                     <label className={`control-label col-sm-2 ${styles.variabel}`}>Curicculum Vitae (CV) </label>
                     <div className={`col-7 ${styles.column}`}>
-                            <input class="form-control" type="file" />
+                            <input className={`form-control ${styles.size}`}  type="file" />
                         </div>
                 </div>
                 <div class="row mb-3">
                     <label className={`control-label col-sm-2 ${styles.variabel}`}>Scan Ijazah </label>
                     <div className={`col-7 ${styles.column}`}>
-                            <input class="form-control" type="file"/>
+                            <input className={`form-control ${styles.size}`}  type="file"/>
                         </div>
                 </div>
                 <div class="row mb-3">
                     <label className={`control-label col-sm-2 ${styles.variabel}`}>Scan Kartu Tanda Penduduk (KTP) </label>
                     <div className={`col-7 ${styles.column}`}>
-                            <input class="form-control" type="file"/>
+                            <input className={`form-control ${styles.size}`}  type="file"/>
                         </div>
                 </div>
                 <div class="m-3 text-center">
@@ -55,6 +71,7 @@ class Pendaftaran extends Component {
                 </div>
             </form>
         </div>
+        
       </React.Fragment>
     );
   }
--- src/components/pages/PendaftaranBerhasil.jsx
+++ src/components/pages/PendaftaranBerhasil.jsx
@@ -13,47 +13,7 @@ class PendaftaranBerhasil extends Component {
             <div class="mb-3 row text-center">
                 <h1>Pendaftaran</h1>
             </div>
-            <form class="form-horizontal">
-                <div class="row mb-3">
-                    <label className={`control-label col-sm-2 ${styles.width}`}>Nama Lengkap</label>
-                        <div className={`col-7 ${styles.column}`}>
-                            <input class="form-control" type="Nama Lengkap" />
-                        </div>
-                </div>
-                <div class="row mb-3">
-                    <label className={`control-label col-sm-2 ${styles.width}`}>Nomor Telepon (Whatsapp) </label>
-                    <div className={`col-7 ${styles.column}`}>
-                            <input class="form-control" type="Nomor telepon" />
-                        </div>
-                </div>
-                <div class="row mb-3">
-                    <label className={`control-label col-sm-2 ${styles.width}`}>Scan Pas Foto (3x4) </label>
-                    <div className={`col-7 ${styles.column}`}>
-                            <input class="form-control" type="Nomor telepon" />
-                        </div>
-                </div>
-                <div class="row mb-3">
-                    <label className={`control-label col-sm-2 ${styles.width}`}>Curicculum Vitae (CV) </label>
-                    <div className={`col-7 ${styles.column}`}>
-                            <input class="form-control" type="Nomor telepon" />
-                        </div>
-                </div>
-                <div class="row mb-3">
-                    <label className={`control-label col-sm-2 ${styles.width}`}>Scan Ijazah </label>
-                    <div className={`col-7 ${styles.column}`}>
-                            <input class="form-control" type="Nomor telepon" />
-                        </div>
-                </div>
-                <div class="row mb-3">
-                    <label className={`control-label col-sm-2 ${styles.width}`}>Scan Kartu Tanda Penduduk (KTP) </label>
-                    <div className={`col-7 ${styles.column}`}>
-                            <input class="form-control" type="Nomor telepon" />
-                        </div>
-                </div>
-                <div class="m-3 text-center">
-                  <button className={`btn btn-success ${styles.width}`}>Submit</button>
-                </div>
-            </form>
+            
         </div>
       </React.Fragment>
     );
