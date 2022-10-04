3147349055d1e0505d5ad8721dd37a0661ca7712
claratanudy <clara.535200025.stu.untar.ac.id>
2022-10-04 19:27:11 +0700

    homepage finish

--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import styles from '../static/css/homepage.module.css';
-import Logo from '../static/images/logo.png';
-import Leader from '../static/images/pimpinan.jpg';
+// import Logo from '../static/images/logo.png';
+// import Leader from '../static/images/pimpinan.jpg';
+// import * as images from '../static/images';
+
+function importAll(r) {
+  let images= {};
+  r.keys().map(item => { images[item.replace('../static/images')] = r(item); });
+  return images;
+}
+
+
 
 class HomePage extends Component {
   constructor(props) {
@@ -43,6 +54,30 @@ class HomePage extends Component {
       margin: '10px',
     };
 
+    const responsive = {
+      superLargeDesktop: {
+        // the naming can be any, depends on you.
+        breakpoint: { max: 4000, min: 3000 },
+        items: 5
+      },
+      desktop: {
+        breakpoint: { max: 3000, min: 1024 },
+        items: 3
+      },
+      tablet: {
+        breakpoint: { max: 1024, min: 464 },
+        items: 2
+      },
+      mobile: {
+        breakpoint: { max: 464, min: 0 },
+        items: 1
+      }
+    };
+
+    const images = importAll(require.context('../static/images', false, /\.png$/));
+    const imagesJPG = importAll(require.context('../static/images', false, /\.jpg$/));
+
+    console.log(images);
     return (
       <React.Fragment>
         {/* <h1 style={{margin: "10px"}}>HomePage</h1>
@@ -54,8 +89,8 @@ class HomePage extends Component {
         <div className={styles.bghome}> 
           <div class="container">
             <div class="row" className={styles.jarak}>
-                <h4>Selamat datang di <img src={Logo} style={{marginBottom:'0.5%'}} width="260vw" height="33vh"/></h4>
-                <p><img src={Logo} style={{marginBottom:'0.4%'}} width="125vw" height="16vh"/> adalah Lembaga Pelatihan Keahlian yang membimbing calon peserta uji kompetisi PR (Public Relations)
+                <h4>Selamat datang di <img src={images['./logo.png']} className={styles.imgLogo}/>!</h4>
+                <p><img src={images['./logo.png']} className={styles.imgTxtLogo}/> adalah Lembaga Pelatihan Keahlian yang membimbing calon peserta uji kompetisi PR (Public Relations)
                   untuk melaksanakan sertifikasi profesi PR dari Badan Nasional Sertifikasi Profesi.
                 </p>
                 <center><button className={styles.pendaftaran}>PENDAFTARAN</button></center>
@@ -65,24 +100,50 @@ class HomePage extends Component {
 
         <div className={styles.enter}>
           <div class="container">
-            <h4 className={styles.enter}>Apa itu <img src={Logo} style={{marginBottom:'0.5%'}} width="260vw" height="33vh"/> ?</h4>
-            <p> <img src={Logo} style={{marginBottom:'0.3%'}} width="120vw" height="15vh"/> adalah lembaga yang mempersiapkan para calon peserta uji kompetensi PR (Public Relations) sebelum melaksanakan
+            <h4 className={styles.enter}>Apa itu <img src={images['./logo.png']} className={styles.imgLogo}/>?</h4>
+            <p> <img src={images['./logo.png']} className={styles.imgTxtLogo}/> adalah lembaga yang mempersiapkan para calon peserta uji kompetensi PR (Public Relations) sebelum melaksanakan
               uji kompetensi dari Lembaga Sertifikasi Profesi Public Relations Indonesia (LSP PRI) atas nama Badan Nasional 
-              Sertifikasi Profesi (BNSP). <img src={Logo} style={{marginBottom:'0.3%'}} width="120vw" height="15vh"/> mendaftarkan para calon peserta pada 
+              Sertifikasi Profesi (BNSP). <img src={images['./logo.png']} className={styles.imgTxtLogo}/> mendaftarkan para calon peserta pada 
               LSP PRI untuk mengikuti uji kompetensi PR.
             </p>
 
             
-            <h4 className={styles.enter} >Pimpinan <img src={Logo} style={{marginBottom:'0.5%'}} width="260vw" height="33vh"/></h4>
-            <img src={Leader} style={{marginTop:'2%'}} class="rounded mx-auto d-block"width="700vw" height="400vh"/>
+            <h4 className={styles.enter} >Pimpinan <img src={images['./logo.png']} className={styles.imgLogo}/></h4>
+            <img src={imagesJPG['./pimpinan.jpg']} style={{marginTop:'2%'}} class="rounded mx-auto d-block" width="700vw" height="400vh"/>
             <p style={{marginBottom:'2%'}}>Dr. Muhammad Adi Pribadi S.E.,M.IB.,M.Com.,C.PR</p>
             <p>
-              Dr. Muhammad Adi Pribadi adalah pimpinan <img src={Logo} style={{marginBottom:'0.3%'}} width="120vw" height="15vh"/> yang berperan sebagai pembimbing teknis persiapan sertifikasi profesi kepada calon peserta uji kompetensi. Beliau adalah asesor aktif di LSP PRI
+              Dr. Muhammad Adi Pribadi adalah pimpinan <img src={images['./logo.png']} className={styles.imgTxtLogo}/> yang berperan sebagai pembimbing teknis persiapan sertifikasi profesi kepada calon peserta uji kompetensi. Beliau adalah asesor aktif di LSP PRI
               (Lembaga Sertifikasi Profesi Public Relations Indonesia). Saat ini beliau memiliki sertifikat ASESOR dan Certified Public Relations dari LSP PRI - BNSP.
               Aktifitas lainnya, beliau adalah seorang dosen tetap di Universitas Tarumanagara, yang juga aktif di organisasi Ikatan Doktor Ilmu Komunikasi 
               Universitas Padjajaran (IDIK Unpad) sebagai wakil sekjen.
             </p>
+          </div>
+        </div>
 
+        <div className={styles.enter}>
+          <div class="container">
+            <Carousel
+            swipeable={false}
+            draggable={false}
+            showDots={true}
+            responsive={responsive}
+            ssr={true} // means to render carousel on server-side.
+            infinite={true}
+            autoPlay={this.props.deviceType !== "mobile" ? true : false}
+            autoPlaySpeed={2000}
+            keyBoardControl={true}
+            customTransition="all .2"
+            transitionDuration={500}
+            containerClass="carousel-container"
+            removeArrowOnDeviceType={["tablet", "mobile", "desktop"]}
+            deviceType={this.props.deviceType}
+            dotListClass="custom-dot-list-style"
+            >
+              <div><img className={styles.imgAds} src={images['./1.png']} /></div>
+              <div><img className={styles.imgAds} src={images['./2.png']} /></div>
+              <div><img className={styles.imgAds} src={images['./3.png']} /></div>
+              <div><img className={styles.imgAds} src={images['./4.png']} /></div>
+            </Carousel>
           </div>
         </div>
       </React.Fragment>
